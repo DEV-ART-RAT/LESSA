0315adf42c6efe552d7cedf61fddd6155ae5ab52
CristianAlfaro <00167917@uca.edu.sv>
2019-06-10 19:08:09 -0600

    recycler view, view model, fragemento home, base de datos, room

--- app/src/main/java/com/e_sea_Ruby/e_sea_language/Fragments/Home_Fragment.kt
+++ app/src/main/java/com/e_sea_Ruby/e_sea_language/Fragments/Home_Fragment.kt
@@ -1,37 +1,114 @@
 package com.e_sea_Ruby.e_sea_language.Fragments
 
 
+import android.content.Context
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.e_sea_Ruby.e_sea_language.R
+import com.e_sea_Ruby.e_sea_language.ViewModel.WordViewModel
+import com.example.myapplication.Adapter.WordAdapter
+import kotlinx.android.synthetic.main.fragment_home_.view.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
-class Home_Fragment : Fragment() {
+
+class Home_Fragment : Fragment(){
+
+
+    private var listener: OnFragmentInteractionListener? = null
+    private lateinit var wordViewModel: WordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+
+    }
 
     override fun onCreateView(
+
+
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_, container, false)
+        var view =  inflater.inflate(R.layout.fragment_home_, container, false)
+        initAll(view)
+        return view
     }
+
+
+
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    interface OnFragmentInteractionListener {
+
+        fun onFragmentInteraction(uri: Uri)
+    }
+
     companion object {
         @JvmStatic
         fun newInstance() =
-            Home_Fragment()
+            Home_Fragment().apply {
+                arguments = Bundle().apply {
+                }
+            }
+    }
+
+    fun initAll(view: View) {
+
+
+        val recyclerView = view.recyclerview
+        val adapter = object : WordAdapter(view.context) {
+            override fun addListener(
+                holder: WordViewHolder,
+                palabra: String,
+                Categoria: String,
+                seña: Int,
+                favorito: Boolean,
+                Status: Boolean
+            ) {
+                holder.word_container.setOnClickListener {
+
+                }
+            }
+
+        }
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+
+
+        wordViewModel = ViewModelProviders.of(this).get(WordViewModel::class.java)
+
+        wordViewModel.allWords.observe(this, Observer { words ->
+            // Update the cached copy of the words in the adapter.
+            words?.let { adapter.setWords(it) }
+        })
 
     }
 
 }
+
