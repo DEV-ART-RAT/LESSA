93fca916a98c979cb1e4ef20a9c22a00f7044117
DARUBI <00121117@uca.edu.sv>
2019-06-13 11:02:32 -0600

    Rebuild in limpio

--- app/src/main/java/com/DevRAT/lessa/MainActivity.kt
+++ app/src/main/java/com/DevRAT/lessa/MainActivity.kt
@@ -1,37 +1,43 @@
-package com.DevRAT.lessa
+package com.sergiomarrero.kotlineando
 
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import android.support.v4.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        bottom_navigation_view.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_music -> {
+                    val fragment = MusicFragment.newInstance()
+                    openFragment(fragment)
+                    true
+                }
+                R.id.action_films -> {
+                    val fragment = FilmsFragment.newInstance()
+                    openFragment(fragment)
+                    true
+                }
+                R.id.action_books -> {
+                    val fragment = BooksFragment.newInstance()
+                    openFragment(fragment)
+                    true
+                }
+                else -> false
+            }
+        }
+        bottom_navigation_view.selectedItemId = R.id.action_music
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.main_container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
