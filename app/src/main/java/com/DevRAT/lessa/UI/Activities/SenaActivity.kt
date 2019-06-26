137877784c348e26c589497b057ab827c8e1739d
luist23 <00127717@uca.edu.sv>
2019-06-26 11:44:29 -0600

    modificando ciertos detalles esteticos.... y carga de gifde red :v

--- app/src/main/java/com/DevRAT/lessa/UI/Activities/SenaActivity.kt
+++ app/src/main/java/com/DevRAT/lessa/UI/Activities/SenaActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.DevRAT.lessa.Database.Entities.Senas
 import com.DevRAT.lessa.R
+import com.sovize.ultracop.controlers.network.Glider
 import kotlinx.android.synthetic.main.activity_sena.*
 
 class SenaActivity : AppCompatActivity() {
@@ -14,6 +15,7 @@ class SenaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sena).apply {
+            Glider.load("https://firebasestorage.googleapis.com/v0/b/lessa-c8cf6.appspot.com/o/senas%2Fgif_hola.gif?alt=media&token=dcb56b97-9fed-4f4c-a074-a584df754b63",gifImageView)
             textView2.text = sena?.palabra
         }
 
